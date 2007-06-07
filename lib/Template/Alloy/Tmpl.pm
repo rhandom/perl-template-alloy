a8ea648e8f1747a348c6605ef9ff6308cf0770fa
paul <paul>
2007-06-07 21:28:23 +0000

    update

--- lib/Template/Alloy/Tmpl.pm
+++ lib/Template/Alloy/Tmpl.pm
@@ -2,22 +2,7 @@ package Template::Alloy::Tmpl;
 
 =head1 NAME
 
-Template::Alloy::Tmpl - provide Text::Tmpl support
-
-=head1 DESCRIPTION
-
-Provides for extra or extended features that may not be as commonly used.
-This module should not normally be used by itself.
-
-See the Template::Alloy documentation for configuration and other parameters.
-
-=head1 AUTHOR
-
-Paul Seamons <paul at seamons dot com>
-
-=head1 LICENSE
-
-This module may be distributed under the same terms as Perl itself.
+Template::Alloy::Tmpl - Text::Tmpl role
 
 =cut
 
@@ -27,6 +12,10 @@ use Template::Alloy;
 
 our $VERSION = $template::Alloy::VERSION;
 our $error;
+
+sub new { die "This class is a role for use by packages such as Template::Alloy" }
+
+###----------------------------------------------------------------###
 
 sub parse_tree_tmpl {
     my $self = shift;
@@ -41,10 +30,6 @@ sub parse_tree_tmpl {
 
     return $self->parse_tree_tt3(@_);
 }
-
-###----------------------------------------------------------------###
-
-
 
 ###----------------------------------------------------------------###
 ### support for few Text::Tmpl calling syntax
@@ -108,3 +93,94 @@ sub fetch_loop_iteration { shift->loop_iteration(@_) }
 ###----------------------------------------------------------------###
 
 1;
+
+__END__
+
+=head1 DESCRIPTION
+
+The Template::Alloy::Tmpl role provides the syntax and the interface
+for Text::Tmpl.  It also brings many of the features from the various
+templating systems.
+
+See the Template::Alloy documentation for configuration and other parameters.
+
+=head1 ROLE_METHODS
+
+=over 4
+
+=item C<parse_tree_tmpl>
+
+Called by parse_tree when syntax is set to tmpl.  Parses for tags Text::Tmpl style.
+
+=item C<set_delimiters>
+
+Sets the START_TAG and END_TAG to use for parsing.
+
+    $obj->set_delimiters('#[', ']#');
+
+=item C<strerror>
+
+Can be used for checking the error when compile fails (or you can use ->error).
+May be called as function or method (Text::Tmpl only allows as function).
+
+=item C<set_strip>
+
+Determines if trailing whitespace on same line is removed.  Default is false.
+
+=item C<set_dir>
+
+Sets the path to look for included templates in.
+
+=item C<set_value>
+
+Sets a single value that will be used during processing of the template.
+
+    $obj->set_value(key => $value);
+
+=item C<set_values>
+
+Sets multiple values for use during processing.
+
+    $obj->set_values(\%values);
+
+=item C<parse_string>
+
+Processes the passed string.
+
+    my $out = $obj->process_string("#[echo $foo]#");
+
+=item C<parse_file>
+
+Processes the passed filename.
+
+    my $out = $obj->process_file("my/file.tmpl");
+
+=item C<loop_iteration>
+
+Same as the Text::Tmpl method - used for adding iterations to a loop.
+
+    my $ref = $obj->loop_iteration('loop1'); # creates iteration 0
+    $ref->set_values($hash);
+
+=item C<fetch_loop_iteration>
+
+Gets a previously created loop iteration.
+
+    my $ref = $obj->fetch_loop_iteration('loop1', 0);
+    $ref->set_values($hash);
+
+=back
+
+=head1 UNSUPPORTED Text::Tmpl METHODS
+
+register_simple, register_pair, alias_simple, alias_pair, remove_simple, remove_pair, set_debug, errno
+
+=head1 AUTHOR
+
+Paul Seamons <paul at seamons dot com>
+
+=head1 LICENSE
+
+This module may be distributed under the same terms as Perl itself.
+
+=cut
