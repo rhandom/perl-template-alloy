2508ff3aade034bb45ffe1054a366a4d0dd50b11
paul <paul>
2007-10-05 16:33:40 +0000

    up version

--- lib/Template/Alloy.pm
+++ lib/Template/Alloy.pm
@@ -14,7 +14,7 @@ use Template::Alloy::VMethod  qw(define_vmethod $SCALAR_OPS $FILTER_OPS $LIST_OP
 
 use vars qw($VERSION);
 BEGIN {
-    $VERSION            = '1.006';
+    $VERSION            = '1.007';
 };
 our $QR_PRIVATE         = qr/^[_.]/;
 our $WHILE_MAX          = 1000;
